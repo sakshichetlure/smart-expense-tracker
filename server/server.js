f625b80d647dfdb302961ab1a153173e114f96cc
username <email_address>
2026-09-07 22:17:22 +0530

    Use bcrypt instead of bcryptjs

--- server/server.js
+++ server/server.js
@@ -118,7 +118,7 @@ pool.query("SELECT id, name, email FROM users").then(res => {
   console.log("=== REGISTERED USERS ===");
   console.table(res.rows);
 }).catch(err => console.error("Users fetch error:", err.message));
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 (async () => {
   const hash = await bcrypt.hash('123456', 10);
   await pool.query("UPDATE users SET password = $1 WHERE email = 'sakshi_final@gmail.com'", [hash]);
