22710a3a16d048ae903a6b213725b50c78c8a953
username <email_address>
2026-09-08 12:36:06 +0530

    Add missing GET / route in budgetRoutes

--- server/routes/budgetRoutes.js
+++ server/routes/budgetRoutes.js
@@ -6,5 +6,5 @@ router.post('/set', budgetController.setBudget);
 router.post('/', budgetController.setBudget);
 router.get('/status/:userId', budgetController.getBudgetStatus);
 router.get("/mom-comparison/:userId", budgetController.getMoMComparison);
-
+router.get('/', budgetController.getBudgetStatus);
 module.exports = router;
